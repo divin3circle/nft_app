12cd13329814dc9b37aae571a3cca20759b43d41
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-04-22 19:15:03 +0300

    NFT app init

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,31 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    background: "#fff",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#000",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
   },
+};
+
+export const appColors = {
+  light: {
+    theme: "#191819",
+    themeEnd: "#212121",
+    button: "#767677",
+    buttonEnd: "#242424",
+    primary: "#feb423",
+    primaryEnd: "#fec52e",
+  },
+  dark: {},
 };
