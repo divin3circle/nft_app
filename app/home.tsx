6d286fccbd99017459161aa795bf827b6a043741
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-04-23 10:02:49 +0300

    Update dependencies and add new packages, finished first half of home screen

--- app/home.tsx
+++ app/home.tsx
@@ -2,33 +2,88 @@ import {
   SafeAreaView,
   StyleSheet,
   Text,
+  View,
   StatusBar,
-  BackHandler,
-  Alert,
+  TextInput,
+  Pressable,
+  TouchableOpacity,
+  FlatList,
 } from "react-native";
 import React, { useEffect } from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import { appColors } from "@/constants/Colors";
+import { Entypo } from "@expo/vector-icons";
+import { FlashList } from "@shopify/flash-list";
+import { filters } from "./utils/Data";
+import { Ionicons } from "@expo/vector-icons";
+
+function FilterItem({
+  item,
+  selectedFilter,
+}: {
+  item: {
+    id: number;
+    name: string;
+  };
+  selectedFilter: number;
+}) {
+  return (
+    <LinearGradient
+      colors={
+        selectedFilter === item.id
+          ? [appColors.app.button, appColors.app.buttonEnd]
+          : ["transparent", "transparent"]
+      }
+      start={{ x: 0.1, y: 0.1 }}
+      end={{ x: 1, y: 1 }}
+      style={{
+        borderRadius: 25,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 5,
+        marginHorizontal: 5,
+      }}
+    >
+      <Text
+        style={{
+          color: appColors.app.text,
+          fontSize: 16,
+          fontFamily: "SpaceMono",
+          marginHorizontal: 10,
+        }}
+      >
+        {item.name}
+      </Text>
+    </LinearGradient>
+  );
+}
 
 const Home = () => {
-  const backAction = () => {
-    Alert.alert("Hold on!", "Are you sure you want to go exit?", [
-      {
-        text: "Cancel",
-        onPress: () => null,
-        style: "cancel",
-      },
-      { text: "YES", onPress: () => BackHandler.exitApp() },
-    ]);
-    return true;
+  const [selectedFilter, setSelectedFilter] = React.useState(1);
+  const flatListRef = React.useRef<FlatList | null>(null);
+  const [selectedItemIndex, setSelectedItemIndex] = React.useState(0);
+
+  const handleFilter = (id: number, index: number) => {
+    setSelectedFilter(id);
+    setSelectedItemIndex(index);
+    // if (flatListRef.current) {
+    //   flatListRef.current.scrollToIndex({
+    //     animated: true,
+    //     viewOffset: 0,
+    //     index: id - 1,
+    //   });
+    // }
   };
 
-  useEffect(() => {
-    const backHandler = BackHandler.addEventListener(
-      "hardwareBackPress",
-      backAction
-    );
-  }, []);
+  React.useEffect(() => {
+    if (flatListRef.current) {
+      flatListRef.current.scrollToIndex({
+        index: selectedItemIndex,
+        animated: true,
+      });
+    }
+  }, [selectedFilter]);
+
   return (
     <LinearGradient
       colors={[appColors.app.theme, appColors.app.themeEnd]}
@@ -40,18 +95,162 @@ const Home = () => {
       }}
     >
       <StatusBar barStyle="light-content" />
-      <SafeAreaView>
-        <Text
+      <SafeAreaView
+        style={{
+          flex: 1,
+          width: "100%",
+          alignItems: "center",
+        }}
+      >
+        {/* user */}
+        <View
           style={{
-            fontSize: 20,
-            fontWeight: "bold",
-            textAlign: "center",
-            marginTop: 20,
-            color: appColors.app.text,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: 20,
+            width: "100%",
           }}
         >
-          Home
-        </Text>
+          <View>
+            <Text
+              style={{
+                color: appColors.app.text,
+                fontSize: 20,
+                fontFamily: "SpaceMono",
+              }}
+            >
+              Hello👋!
+            </Text>
+            <Text
+              style={{
+                color: appColors.app.text,
+                fontSize: 26,
+                fontWeight: "bold",
+                fontFamily: "SpaceMono",
+              }}
+            >
+              Sylus Abel
+            </Text>
+          </View>
+          <LinearGradient
+            colors={[appColors.app.button, appColors.app.buttonEnd]}
+            style={{
+              borderRadius: 50,
+              padding: 10,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            start={{ x: 0.1, y: 0.1 }}
+            end={{ x: 1, y: 1 }}
+          >
+            <Entypo name="menu" size={30} color={appColors.app.text} />
+          </LinearGradient>
+        </View>
+        {/* search */}
+        <View
+          style={{
+            flexDirection: "row",
+            gap: 10,
+            alignItems: "center",
+            width: "80%",
+            borderColor: appColors.app.text,
+            borderWidth: StyleSheet.hairlineWidth,
+            borderRadius: 50,
+            padding: 10,
+            marginVertical: 20,
+          }}
+        >
+          <Entypo
+            name="magnifying-glass"
+            size={30}
+            color={appColors.app.button}
+          />
+          <TextInput
+            placeholder="Search"
+            placeholderTextColor={appColors.app.button}
+            style={{
+              color: appColors.app.button,
+              fontSize: 14,
+              fontFamily: "SpaceMono",
+              width: "75%",
+            }}
+          />
+        </View>
+        {/* filters */}
+        <View
+          style={{
+            marginVertical: 20,
+            height: 30,
+          }}
+        >
+          <FlatList
+            key={selectedFilter}
+            data={filters}
+            renderItem={({ item, index }) => (
+              <TouchableOpacity
+                key={item.id}
+                onPress={() => handleFilter(item.id, index)}
+              >
+                <FilterItem item={item} selectedFilter={selectedFilter} />
+              </TouchableOpacity>
+            )}
+            keyExtractor={(item) => item.id.toString()}
+            // estimatedItemSize={157}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            getItemLayout={(data, index) => ({
+              length: 8,
+              offset: 8 * index,
+              index,
+            }) // replace ITEM_SIZE with your item size
+            }
+          />
+        </View>
+        {/* trending */}
+        <View
+          style={{
+            width: "100%",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            paddingHorizontal: 20,
+          }}
+        >
+          <Text
+            style={{
+              color: appColors.app.text,
+              fontSize: 22,
+              fontFamily: "SpaceMono",
+            }}
+          >
+            Trending Collection
+          </Text>
+          <Pressable
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Text
+              style={{
+                color: appColors.app.button,
+                fontSize: 13,
+                fontFamily: "SpaceMono",
+              }}
+            >
+              This Week
+            </Text>
+            <Ionicons
+              name="chevron-forward-outline"
+              size={20}
+              color={appColors.app.button}
+            />
+          </Pressable>
+        </View>
+        {/* nfts */}
+        <View></View>
       </SafeAreaView>
     </LinearGradient>
   );
---
+++ app/utils/Data.ts
@@ -0,0 +1,36 @@
+const filters = [
+  {
+    id: 1,
+    name: "All",
+  },
+  {
+    id: 2,
+    name: "Design",
+  },
+  {
+    id: 3,
+    name: "Art",
+  },
+  {
+    id: 4,
+    name: "Membership",
+  },
+  {
+    id: 5,
+    name: "Gaming",
+  },
+  {
+    id: 6,
+    name: "Photography",
+  },
+  {
+    id: 7,
+    name: "Music",
+  },
+  {
+    id: 8,
+    name: "Tech",
+  },
+];
+
+export { filters };
